ff0e82326ee520b22524adc15058d02c64fd2b06
keento0809 <united0102hand7512@gmail.com>
2020-11-02 11:41:55 +0000

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -19,6 +19,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
@@ -32,7 +34,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
