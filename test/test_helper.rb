701fad52532220e0ef70d0d6cfb0240b902f97be
keento0809 <united0102hand7512@gmail.com>
2020-11-07 11:19:27 +0000

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,7 +15,7 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーとしてログインする
   def log_in_as(user)
     session[:user_id] = user.id
   end
